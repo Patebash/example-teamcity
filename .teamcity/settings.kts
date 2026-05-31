1c8726a016e915b2016a7e4a1c2764fe2b0f11d6
admin <admin@TeamCity>
2026-05-31 16:27:31 +0000

    runners of 'Build' build configuration were updated (TeamCity change in 'netology' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,7 +44,7 @@ object Build : BuildType({
             conditions {
                 doesNotContain("teamcity.build.branch", "master")
             }
-            goals = "clean deploy"
+            goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             userSettingsSelection = "settings.xml"
         }
